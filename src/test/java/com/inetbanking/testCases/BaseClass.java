53b6a96c5d5738f79084febc139cbcb26e0e9f51
shriram mohalkar <mohalkar.ram@gmail.com>
2023-10-07 13:06:04 +0530

    ada

--- src/test/java/com/inetbanking/testCases/BaseClass.java
+++ src/test/java/com/inetbanking/testCases/BaseClass.java
@@ -49,7 +49,7 @@ public class BaseClass {
 		{
 			System.setProperty("webdriver.chrome.driver",readconfig.getChromePath());
 			//WebDriverManager.chromedriver().setup(); 
-			// added remote brnch
+			// added remote jay hari sa
 			driver=new ChromeDriver(options);
 		}
 		else if(br.equals("firefox"))
